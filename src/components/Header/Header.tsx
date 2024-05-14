a095c9aaedf56d902d14b0450210aa219a01f4db
YuriyDPK <samp_samp96@bk.ru>
2024-05-14 13:04:45 +0300

    ервоначальный коммит

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="p-5 flex justify-between align-middle">
+      <Link
+        href="/"
+        className="text-3xl"
+        style={{ fontFamily: "Viaoda Libre", fontWeight: 100 }}
+      >
+        Y.P.
+      </Link>
+      <div className="flex gap-10">
+        <Link
+          href="/"
+          className="text-2xl"
+          style={{ fontFamily: "Roboto Flex", fontWeight: 100 }}
+        >
+          Главная
+        </Link>
+        <Link
+          href="/"
+          className="text-2xl"
+          style={{ fontFamily: "Roboto Flex", fontWeight: 100 }}
+        >
+          Работы
+        </Link>
+        <Link
+          href="/"
+          className="text-2xl"
+          style={{ fontFamily: "Roboto Flex", fontWeight: 100 }}
+        >
+          Магазин
+        </Link>
+        <Link
+          href="/"
+          className="text-2xl"
+          style={{ fontFamily: "Roboto Flex", fontWeight: 100 }}
+        >
+          Обо мне
+        </Link>
+      </div>
+    </div>
+  );
+}
